16d4c69d9751cf2c7fab0de4220f45ae4d6aaf34
Sam Greenlee <sam.a.greenlee@gmail.com>
2015-10-06 15:28:15 -0400

    C Primer Plus:
    Created .gitignore file.
    Added ch3 README.
    Did ch3 exercise 1.

---
+++ ch3/README.md
@@ -0,0 +1,4 @@
+Chapter 3: Data and C
+======================
+
+Introduction to basic C data types.
---
+++ ch3/exercise1.c
@@ -0,0 +1,34 @@
+/* Exercise 1
+
+Find out what your system does with integer overflow, floating-point overflow,
+and floating point underflow by using the experimental approach; that is,
+write programs that have these problems.
+
+*/
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+int main(void)
+{
+	int int_overflow;
+	int MAX_INTEGER = INT_MAX;
+	float flt_overflow, flt_underflow;
+	float MIN_FLOAT = FLT_MIN;
+	float MAX_FLOAT = FLT_MAX;
+	
+	// artificially create over/underflow
+	int_overflow = INT_MAX + 1;
+	flt_overflow = FLT_MAX * 2.;
+	flt_underflow = FLT_MIN / 2.;
+	
+	
+	
+	
+	// print results
+	printf("Max integer: %d \tMax integer + 1: %d\n", INT_MAX, int_overflow);
+	printf("Max float: %f \tMax float * 2: %f\n", FLT_MAX, flt_overflow);
+	printf("Min float: %f \tMin float / 2: %f\n", FLT_MIN, flt_underflow);
+
+	return 1;
+}
