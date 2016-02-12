6554cb4e55e2a53d3c24f1a8cd49c125ff8a0da3
Sam Greenlee <sam.a.greenlee@gmail.com>
2016-02-12 16:38:20 -0500

    Did chapter 16 exercise 1.

---
+++ ch16/exercise01.h
@@ -0,0 +1,14 @@
+// C Primer Plus
+// Chapter 16 Exercise 1:
+
+// Start developing a header file of preprocessor definitions that you want
+// to use.
+
+#ifndef EXERCISE01_H_
+#define EXERCISE01_H_
+
+#include <stdio.h>
+
+#define CLEARINPUT while (getchar() != '\n') continue
+
+#endif
